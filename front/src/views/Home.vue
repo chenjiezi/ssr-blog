2c6871ed383cf774033990ee2d9ed47dafa087f6
chenjiezi <892774940@qq.com>
2020-07-05 23:38:06 +0800

    home页面

--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -1,17 +1,28 @@
 <template>
-  <div class="home">
-    home
+  <div :class="home.home">
+    <div :class="home.container">
+      <div class="articleContainer"></div>
+      <div class="articleContainer"></div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
   }
 }
 </script>
+
+<style lang="stylus" module="home">
+@import '../assets/css/variable.styl'
+.home
+  .container
+    max-width 1080px
+    min-width 320px
+    margin 0 auto
+    padding $space-middle
+    background rgba(0,0,0,.04)
+</style>
