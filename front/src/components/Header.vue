8bcc91017bf1d169b3a0bc3063f03a2b566425a7
chenjiezi <892774940@qq.com>
2020-07-05 23:37:23 +0800

    footer、header组件

--- front/src/components/Header.vue
+++ front/src/components/Header.vue
@@ -1,17 +1,34 @@
 <template>
-  <div>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/archives">Archives</router-link> |
-    <router-link to="/about">About</router-link>
+  <div :class="header.header">
+    <div :class="header.container">
+      <div :class="header.logo">
+        <router-link to="/">Home</router-link>
+      </div>
+      <div :class="header.navBar">
+        <router-link to="/archives">Archives</router-link>
+        <router-link to="/about">About</router-link>
+      </div>
+    </div>
   </div>
 </template>
 
-<script>
-export default {
-  
-}
-</script>
-
 <style lang="stylus" module="header">
-  
+@import '../assets/css/variable.styl'
+.header
+  height 65px
+  border-bottom 1px solid #ccc
+  font-size $font-size-middle
+  .container
+    max-width 1080px
+    min-width 320px
+    height 100%
+    margin 0 auto
+    display flex
+    justify-content space-between
+    align-items center
+    background rgba(0,0,0,.05)
+    padding 0 $space-middle
+    .navBar
+      & > *
+        margin-left $space-middle
 </style>
