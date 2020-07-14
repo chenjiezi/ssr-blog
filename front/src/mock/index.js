85270ba99ead79a6ff836e8736b8a787a382faf4
chenjiezi <892774940@qq.com>
2020-07-14 23:17:34 +0800

    mock data

---
+++ front/src/mock/index.js
@@ -0,0 +1,74 @@
+export const articleList = [
+  {
+    id: 0,
+    title: '我是标题',
+    blogSummary: '啊啊啊啊啊啊啊啊啊啊啊啊',
+    dateTime: '2020年7月11日15点30分',
+  },
+  {
+    id: 1,
+    title: '我是标题1',
+    blogSummary: '啊啊啊啊啊啊啊啊啊啊啊啊',
+    dateTime: '2020年7月11日15点30分',
+  },
+  {
+    id: 2,
+    title: '我是标题2',
+    blogSummary: '啊啊啊啊啊啊啊啊啊啊啊啊',
+    dateTime: '2020年7月11日15点30分',
+  },
+  {
+    id: 3,
+    title: '我是标题3',
+    blogSummary: '啊啊啊啊啊啊啊啊啊啊啊啊',
+    dateTime: '2020年7月11日15点30分',
+  },
+]
+
+export const tagList = [
+  {
+    id: 0,
+    title: '全部',
+    num: 999,
+  },
+  {
+    id: 98,
+    title: 'HTML&CSS',
+    num: 12,
+  },
+  {
+    id: 1,
+    title: 'JACASCRIPT',
+    num: 23,
+  },
+  {
+    id: 99,
+    title: 'VUE',
+    num: 23,
+  },
+  {
+    id: 2,
+    title: 'NODE',
+    num: 120,
+  },
+  {
+    id: 3,
+    title: 'WEBPACK',
+    num: 120,
+  },
+  {
+    id: 4,
+    title: '数据结构&算法',
+    num: 120,
+  },
+  {
+    id: 5,
+    title: '网络',
+    num: 120,
+  },
+  {
+    id: 6,
+    title: '随笔',
+    num: 120,
+  },
+]
