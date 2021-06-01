279e5cc1bebe9fd178dd18f5721ed0350ec5f8af
chenjiezi <892774940@qq.com>
2021-06-01 22:49:06 +0800

    首页静态页面开发

--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -1,18 +1,61 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div id="Home">
+    <div class="container">
+      <h1 class="title">chenJz's Blog</h1>
+      <p>👨‍💻 Web 前端攻城狮</p>
+      <p>✍️ 记录技术成长历程</p>
+      <div class="entry">
+        <a-button
+          @click="go('Note')"
+          class="entry-btn"
+          shape="round"
+          size="large"
+          type="primary">
+            快速浏览
+        </a-button>
+        <a-button
+          @click="go('Archive')"
+          class="entry-btn"
+          shape="round"
+          size="large"
+          type="primary"
+          ghost>
+            进入博客
+        </a-button>
+      </div>
+    </div>
   </div>
 </template>
 
-<script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+<script>
+  export default {
+    name: 'Home',
+    data () {
+      return {
 
-@Component({
-  components: {
-    HelloWorld,
-  },
-})
-export default class Home extends Vue {}
+      }
+    },
+    methods: {
+      go (path) {
+        this.$router.replace(path)
+        this.$root.$emit('menu', path.toLowerCase())
+      }
+    }
+  }
 </script>
+
+<style lang="stylus" scoped>
+  #Home 
+    .container
+      height calc(100vh - 64px)
+      padding-top 200px
+      font-size 16px
+      background #fff
+      & > *
+        text-align center
+      .title
+        font-size 48px
+      .entry
+        .entry-btn
+          margin 20px
+</style>
