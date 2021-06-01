ddce68f23fc8ef1c6690cf5a1df800a308d0963b
chenjiezi <892774940@qq.com>
2021-06-01 22:47:02 +0800

    原js入口不起作用了

--- front/src/main.ts
+++ front/src/main.ts
@@ -1,12 +1,17 @@
-import Vue from 'vue'
-import App from './App.vue'
-import createrouter from './router'
-import createStore from './store'
+// import Vue from 'vue'
+// import App from './App.vue'
+// import createRouter from './router'
+// import createStore from './store'
+// import Antd from 'ant-design-vue'
+// import 'ant-design-vue/dist/antd.css'
 
-Vue.config.productionTip = false
+// Vue.config.productionTip = false
 
-new Vue({
-  router: createrouter(),
-  store: createStore(),
-  render: h => h(App)
-}).$mount('#app')
+// Vue.use(Antd);
+// console.log('----main-----')
+
+// new Vue({
+//   router: createRouter(),
+//   store: createStore(),
+//   render: h => h(App)
+// }).$mount('#app')
