bcdd2f38e5e069293444e815ae33a396d54d4588
chenjiezi <892774940@qq.com>
2021-06-01 22:47:36 +0800

    引入 Ant Design

--- front/src/app.js
+++ front/src/app.js
@@ -2,19 +2,20 @@ import Vue from 'vue'
 import App from './App.vue'
 import createRouter from './router'
 import createStore from './store'
+import Antd from 'ant-design-vue'
+import 'ant-design-vue/dist/antd.css'
+
+Vue.use(Antd);
 
 export function createApp () {
-  // 创建 router 实例
   const router = createRouter()
   const store = createStore()
 
   const app = new Vue({
-    // 注入 router 到根 Vue 实例
     router,
     store,
     render: h => h(App)
   })
 
-  // 返回 app 和 router
   return { app, router, store }
 }
