f0caccc23222aafd49071c426593fc9ba15bf0ed
chenjiezi <892774940@qq.com>
2021-06-01 22:48:38 +0800

    归档页面开发

---
+++ front/src/views/Archive.vue
@@ -0,0 +1,179 @@
+<template>
+  <div id="Archive">
+    <div class="container">
+      <h1 class="article-w">全部文章（ALL：888）</h1>
+      <div class="list">
+        <h1 class="year">1996</h1>
+        <ul>
+          <li class="month">6月份</li>
+          <li class="title">
+            <a href="//www.baidu.com" target="_blank">Javascript进阶-长任务优化：WebWorker与时间分片</a>
+            <span>1996-09-30</span>
+          </li>
+          <li class="title">
+            <a href="//www.baidu.com" target="_blank">Javascript进阶-长任务优化：WebWorker与时间分片</a>
+            <span>1996-09-30</span>
+          </li>
+          <li class="title">
+            <a href="//www.baidu.com" target="_blank">Javascript进阶-长任务优化：WebWorker与时间分片长任务优化：WebWorker与时间分片长任务优化：WebWorker与时间分片长任务优化：WebWorker与时间分片</a>
+            <span>1996-09-30</span>
+          </li>
+          <li class="title">
+            <a href="//www.baidu.com" target="_blank">Javascript进阶-长任务优化：WebWorker与时间分片长任务优化：WebWorker与时间分片长任务优化：WebWorker与时间分片长任务优化：WebWorker与时间分片</a>
+            <span>1996-09-30</span>
+          </li>
+          <li class="title">
+            <a href="//www.baidu.com" target="_blank">Javascript进阶-长任务优化：WebWorker与时间分片长任务优化：WebWorker与时间分片长任务优化：WebWorker与时间分片长任务优化：WebWorker与时间分片</a>
+            <span>1996-09-30</span>
+          </li>
+        </ul>
+        <ul>
+          <li class="month">8月份</li>
+          <li class="title">
+            <a href="//www.baidu.com" target="_blank">Javascript进阶-长任务优化：WebWorker与时间分片</a>
+            <span>1996-09-30</span>
+          </li>
+          <li class="title">
+            <a href="//www.baidu.com" target="_blank">Javascript进阶-长任务优化：WebWorker与时间分片</a>
+            <span>1996-09-30</span>
+          </li>
+          <li class="title">
+            <a href="//www.baidu.com" target="_blank">Javascript进阶-长任务优化：WebWorker与时间分片长任务优化：WebWorker与时间分片长任务优化：WebWorker与时间分片长任务优化：WebWorker与时间分片</a>
+            <span>1996-09-30</span>
+          </li>
+        </ul>
+      </div>
+      <div class="list">
+        <h1 class="year">1996</h1>
+        <ul>
+          <li class="month">6月份</li>
+          <li class="title">
+            <a href="//www.baidu.com" target="_blank">Javascript进阶-长任务优化：WebWorker与时间分片</a>
+            <span>1996-09-30</span>
+          </li>
+          <li class="title">
+            <a href="//www.baidu.com" target="_blank">Javascript进阶-长任务优化：WebWorker与时间分片</a>
+            <span>1996-09-30</span>
+          </li>
+          <li class="title">
+            <a href="//www.baidu.com" target="_blank">Javascript进阶-长任务优化：WebWorker与时间分片长任务优化：WebWorker与时间分片长任务优化：WebWorker与时间分片长任务优化：WebWorker与时间分片</a>
+            <span>1996-09-30</span>
+          </li>
+        </ul>
+      </div>
+      <div class="list">
+        <h1 class="year">1996</h1>
+        <ul>
+          <li class="month">6月份</li>
+          <li class="title">
+            <a href="//www.baidu.com" target="_blank">Javascript进阶-长任务优化：WebWorker与时间分片</a>
+            <span>1996-09-30</span>
+          </li>
+          <li class="title">
+            <a href="//www.baidu.com" target="_blank">Javascript进阶-长任务优化：WebWorker与时间分片</a>
+            <span>1996-09-30</span>
+          </li>
+          <li class="title">
+            <a href="//www.baidu.com" target="_blank">Javascript进阶-长任务优化：WebWorker与时间分片长任务优化：WebWorker与时间分片长任务优化：WebWorker与时间分片长任务优化：WebWorker与时间分片</a>
+            <span>1996-09-30</span>
+          </li>
+        </ul>
+      </div>
+      <div class="list">
+        <h1 class="year">1996</h1>
+        <ul>
+          <li class="month">6月份</li>
+          <li class="title">
+            <a href="//www.baidu.com" target="_blank">Javascript进阶-长任务优化：WebWorker与时间分片</a>
+            <span>1996-09-30</span>
+          </li>
+          <li class="title">
+            <a href="//www.baidu.com" target="_blank">Javascript进阶-长任务优化：WebWorker与时间分片</a>
+            <span>1996-09-30</span>
+          </li>
+          <li class="title">
+            <a href="//www.baidu.com" target="_blank">Javascript进阶-长任务优化：WebWorker与时间分片长任务优化：WebWorker与时间分片长任务优化：WebWorker与时间分片长任务优化：WebWorker与时间分片</a>
+            <span>1996-09-30</span>
+          </li>
+        </ul>
+      </div>
+      <div class="list">
+        <h1 class="year">1996</h1>
+        <ul>
+          <li class="month">6月份</li>
+          <li class="title">
+            <a href="//www.baidu.com" target="_blank">Javascript进阶-长任务优化：WebWorker与时间分片</a>
+            <span>1996-09-30</span>
+          </li>
+          <li class="title">
+            <a href="//www.baidu.com" target="_blank">Javascript进阶-长任务优化：WebWorker与时间分片</a>
+            <span>1996-09-30</span>
+          </li>
+          <li class="title">
+            <a href="//www.baidu.com" target="_blank">Javascript进阶-长任务优化：WebWorker与时间分片长任务优化：WebWorker与时间分片长任务优化：WebWorker与时间分片长任务优化：WebWorker与时间分片</a>
+            <span>1996-09-30</span>
+          </li>
+        </ul>
+      </div>
+      <!-- 分页 -->
+      <div class="pagination">
+        <a-pagination :total="50" show-less-items />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Archive',
+  }
+</script>
+
+
+<style lang="stylus" scoped>
+  ul
+    list-style none
+
+  #Archive
+    .container
+      width 1080px
+      min-height calc(100vh - 80px)
+      margin 0 auto
+      padding 20px 10px
+      background-color #fff
+      .article-w
+        font-size 35px
+        margin-left 20px
+        color #888
+      .list
+        .year
+          font-size 30px
+          margin-left 20px
+          color #888
+        .month
+          font-weight bold
+          font-size 18px
+          color #888
+        .title
+          display flex
+          cursor pointer
+          font-size 16px
+          transition all 0.3s
+          color #000
+          margin 8px 0
+          margin-left 20px
+          &:hover
+            background rgba(0,0,0,.05)
+            margin-left 22px
+          a
+            display inline-block
+            width 600px
+            overflow hidden
+            white-space nowrap
+            text-overflow ellipsis
+            margin-right 100px
+            color inherit
+    .pagination
+      text-align center
+      margin 20px 0
+</style>
