38cce7118934990ee451ee85607e5f4467c654d1
chenjiezi <892774940@qq.com>
2021-06-03 17:39:48 +0800

    关闭 eslint

--- admin/vue.config.js
+++ admin/vue.config.js
@@ -27,7 +27,7 @@ module.exports = {
   publicPath: '/',
   outputDir: 'dist',
   assetsDir: 'static',
-  lintOnSave: process.env.NODE_ENV === 'development',
+  lintOnSave: false, // 关闭 eslint
   productionSourceMap: false,
   devServer: {
     port: port,
