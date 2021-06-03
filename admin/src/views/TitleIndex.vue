ed729a0f373adf7d2e8e8daf64fb407a6e73135e
chenjiezi <892774940@qq.com>
2021-06-03 17:42:10 +0800

    新增前台侧栏菜单编辑页面

---
+++ admin/src/views/TitleIndex.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="TitleIndex">
+    TitleIndex
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'TitleIndex',
+  }
+</script>
+
+<style lang="scss" scoped>
+  
+</style>
