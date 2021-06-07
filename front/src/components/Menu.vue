62bb97554647faa3a4e71963d66226a6ab37aef3
chenjiezi <892774940@qq.com>
2021-06-07 23:24:54 +0800

    Menu组件逻辑功能迭代

--- front/src/components/Menu.vue
+++ front/src/components/Menu.vue
@@ -34,12 +34,23 @@
   import { getMenuList } from '@/api/menu'
   import { TreeDataFindParents, setLocalData, getLocalData } from '@/util/utils'
 
+  function handleData (data = [], scopedSlots = { title: 'title' }) {
+    data.forEach(item => {
+      if (item.hasContent) {
+        item.scopedSlots = scopedSlots
+      }
+      if (item?.children?.length > 0) {
+        handleData(item.children)
+      }
+    })
+  }
+
   export default {
     name: 'Menu',
     data() {
       return {
         isLoading: true,
-        expandedKeys: [],
+        expandedKeys: getLocalData('expandedKeys') || [],
         selectedKeys: [],
         treeData: []
       }
@@ -48,19 +59,23 @@
       // 获取文章目录列表
       getMenuList()
       .then(res => {
-        this.treeData = res?.data?.menuList || []
+        let data = res?.data?.menuList || []
+        handleData(data)
+        this.treeData = data
         
         // 初始化目录当前节点高亮
         const { params } = this.$router.history.current
         this.selectedKeys = params.id ? [params.id] : []
 
-        // 高亮子节点的父节点如果没展开，会通过计算展开
+        // 高亮的子节点的父节点如果没展开，会通过计算展开
         let p = TreeDataFindParents(this.treeData, params.id)
-        p = p.concat(getLocalData('expandedKeys'))
-        p = [...new Set(p)] // 去重
-        this.expandedKeys = p
-        // 更新本地数据
-        setLocalData('expandedKeys', p)
+        if (p.length > 0) {
+          p = p.concat(getLocalData('expandedKeys'), params.id)
+          p = [...new Set(p)] // 去重
+          this.expandedKeys = p
+          // 更新本地数据
+          setLocalData('expandedKeys', p)
+        }
       })
       .finally(() => {
         this.isLoading = false
@@ -72,19 +87,23 @@
         this.expandedKeys = expandedKeys;
       },
       onSelect(selectedKeys, { node }) {
-        // 点击父节点展开/折叠
+        this.selectedKeys = selectedKeys;
+
         const { dataRef } = node
+
+        // 重定向路由
+        if (dataRef.hasContent) {
+          this.$router.push('/Article/' + dataRef.key).catch(err => err)
+        }
+        
+        // 点击父节点展开/折叠
         let index = this.expandedKeys.indexOf(dataRef.title)
-        if (dataRef.children && dataRef.children.length > 0) {
+        if (dataRef?.children?.length > 0) {
           if (index === -1) {
             this.expandedKeys.push(dataRef.title)
-          } else {
-            this.expandedKeys.splice(index, 1)
+            setLocalData('expandedKeys', this.expandedKeys)
           }
-          setLocalData('expandedKeys', this.expandedKeys)
         }
-
-        this.selectedKeys = selectedKeys;
       },
     },
   }
