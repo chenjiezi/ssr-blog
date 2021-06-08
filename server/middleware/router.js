86ed8d49a273b4c35917e16625976688334335dc
chenjiezi <892774940@qq.com>
2021-06-08 14:01:49 +0800

    server router 逻辑代码编写

--- server/middleware/router.js
+++ server/middleware/router.js
@@ -1,6 +1,7 @@
-const Router = require('koa-router')
 const path = require('path')
+const Mock = require('mockjs')
 
+const Router = require('koa-router')
 const router = new Router()
 
 router.get('/', async (ctx) => {
@@ -8,27 +9,80 @@ router.get('/', async (ctx) => {
   ctx.body = '<h1>你好，菜鸟！</h1>'
 })
 
+// 文章目录
 router.get('/api/menu', async (ctx) => {
-  const filePath = path.join(__dirname, '../menu.js')
+  const filePath = path.join(__dirname, '../mock/menu.js')
 
   try {
-    const data = await require(filePath)
-    ctx.response.body = JSON.stringify({
-      code: 200,
-      message: 'success',
-      data: {
-        menuList: data
-      }
-    })
+    const menuList = await require(filePath)
+    ctx.response.body = resBody({ menuList })
   } catch (error) {
     ctx.response.body = JSON.stringify(error)
   }
 })
 
-router.get('/api/article', (ctx) => {
-  // const filePath = path.join(__dirname, '../menu.js')
-  ctx.response.body = JSON.stringify(ctx)
+// 文章列表
+router.get('/api/article', async (ctx) => {
+  let url = ctx.request.url
+  let params = url.split('?')[1] || ''
+  let curPage = +params.split('=')[1] || 1
+  let pageSize = 10
+  let total = 888
+
+  pageSize = t(curPage - 1, total, pageSize)
+  const articleList = mockList(pageSize)
+
+  ctx.response.body = resBody({
+    articleList: articleList,
+    curPage,
+    pageSize,
+    total
+  })
 })
+
+// 文章详情
+// router.get('/api/article-detail', async (ctx) => {})
+
+function resBody (data, code = 200, message = 'success') {
+  return JSON.stringify({
+    code: code,
+    message: message,
+    data: data
+  })
+}
+
+// mock articleList
+function mockList (pageSize) {
+  const result = []
+  for (let i = 0; i < pageSize; i++) {
+    result.push(Mock.mock({
+      id: '@id',
+      title: '@ctitle(20, 50)',
+      dateTime: '@date',
+      summary: '@cparagraph(5)',
+      path: 'bi-bao',
+      remark: ''
+    }))
+  }
+
+  return result
+}
+
+// 计算当前页的 pageSize
+function t (curPage, total, pageSize) {
+  let q = curPage * pageSize,
+    s = q + pageSize
+
+  if (q < total) {
+    if (s >= total) {
+      pageSize = total - q
+    }
+  } else {
+    pageSize = 0
+  }
+
+  return pageSize
+}
 
 
 module.exports = router
