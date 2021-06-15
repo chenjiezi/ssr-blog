57c7c693779f4e5b4c9e3183bb24e2db2c42ef33
chenjiezi <892774940@qq.com>
2021-06-15 21:08:13 +0800

    引入 axios、stylus

--- front-nuxtjs/nuxt.config.js
+++ front-nuxtjs/nuxt.config.js
@@ -22,7 +22,8 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    '@/plugins/antd-ui'
+    '@/plugins/antd-ui',
+    '@/plugins/request'
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
@@ -32,8 +33,15 @@ export default {
   buildModules: [
   ],
 
+  // publicRuntimeConfig: {
+  //   axios: {
+  //     baseURL: 'http://localhost:8888'
+  //   }
+  // },
+
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
+    '@nuxtjs/axios'
   ],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
