080c118c85064593661513bd9150986f47fc02e4
chenjiezi <892774940@qq.com>
2021-06-20 16:23:17 +0800

    打印请求信息

---
+++ server/middleware/info.js
@@ -0,0 +1,9 @@
+const log = require('log4js').getLogger('server');
+log.level = 'info';
+
+module.exports = async (ctx, next) => {
+  const { request } = ctx
+  log.info(`${ request.method } : ${ request.url } : ${ JSON.stringify(request.body) }`)
+
+  await next()
+}
