c6a36f81b692733ff1a02af54d3bf5ec8fd67d7f
chenjiezi <892774940@qq.com>
2021-06-20 16:38:21 +0800

    逻辑功能优化

--- server/app.js
+++ server/app.js
@@ -1,24 +1,22 @@
 const Koa = require('koa')
-
-const log = require('log4js').getLogger('Mock Server');
-log.level = 'info';
-
 const app = new Koa()
-
+const router = require('./middleware/router')
+const log = require('log4js').getLogger('server')
+const bodyParser = require('koa-bodyparser')
 const respDurationMiddleware = require('./middleware/duration')
 const respHeaderMiddleware = require('./middleware/header')
-// const respDataMiddleware = require('./middleware/data')
-const router = require('./middleware/router')
+const requestInfoMiddleware = require('./middleware/info')
 
-// 第一层中间件：计算服务器相应耗时
-app.use(respDurationMiddleware)
-// 第二层中间件：设置响应头部header
-app.use(respHeaderMiddleware)
-// 第三层中间件：处理业务逻辑
-// app.use(respDataMiddleware)
+log.level = 'info'
 
-app.use(router.routes())
-app.use(router.allowedMethods())
+app
+  .use(bodyParser()) // http 实体中的数据解析到 koa 上下文中
+  .use(respDurationMiddleware) // 计算服务器相应耗时
+  .use(respHeaderMiddleware) // 设置响应头部header
+  .use(requestInfoMiddleware) // 打印请求信息
+  .use(router.routes()) // 注册路由
+  .use(router.allowedMethods())
+
 
 // 启动服务
 app.listen('8888', () => {
