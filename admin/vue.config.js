1ddab2efab3b85044e715c5007b0fd4bbb33d0b7
chenjiezi <892774940@qq.com>
2021-06-28 17:34:15 +0800

    代理服务端接口，去除脚手架自带的mockserver

--- admin/vue.config.js
+++ admin/vue.config.js
@@ -36,7 +36,12 @@ module.exports = {
       warnings: false,
       errors: true
     },
-    before: require('./mock/mock-server.js')
+    // before: require('./mock/mock-server.js'),
+    proxy: {
+      '/api/': {
+        target: 'http://localhost:8888'
+      }
+    }
   },
   configureWebpack: {
     // provide the app's title in webpack's name field, so that
