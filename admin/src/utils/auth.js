012142fc9d6e5560792883a11a8a863d0c4474c5
chenjiezi <892774940@qq.com>
2021-06-28 17:36:51 +0800

    cookie二次封装

--- admin/src/utils/auth.js
+++ admin/src/utils/auth.js
@@ -1,15 +1,14 @@
 import Cookies from 'js-cookie'
 
-const TokenKey = 'vue_admin_template_token'
 
 export function getToken() {
-  return Cookies.get(TokenKey)
+  return Cookies.get('token')
 }
 
 export function setToken(token) {
-  return Cookies.set(TokenKey, token)
+  return Cookies.set('token', token)
 }
 
 export function removeToken() {
-  return Cookies.remove(TokenKey)
+  return Cookies.remove('token')
 }
