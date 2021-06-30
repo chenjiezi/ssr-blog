b24749ced9e5936343c99afd9062c34150eb461c
chenjiezi <892774940@qq.com>
2021-06-30 17:15:46 +0800

    elementui去除i18n

--- admin/src/main.js
+++ admin/src/main.js
@@ -4,7 +4,6 @@ import 'normalize.css/normalize.css' // A modern alternative to CSS resets
 
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-import locale from 'element-ui/lib/locale/lang/en' // lang i18n
 
 import '@/styles/index.scss' // global css
 
@@ -17,10 +16,7 @@ import '@/permission' // permission control
 
 import * as filters from './filters' // global filters
 
-// set ElementUI lang to EN
-Vue.use(ElementUI, { locale })
-// 如果想要中文版 element-ui，按如下方式声明
-// Vue.use(ElementUI)
+Vue.use(ElementUI)
 
 // register global utility filters
 Object.keys(filters).forEach(key => {
