dc0393c7b343b47bbae04102701c13c64c7a40c2
chenjiezi <892774940@qq.com>
2021-07-27 23:38:18 +0800

    add：配置CORS

--- server/middleware/header.js
+++ server/middleware/header.js
@@ -2,6 +2,7 @@ module.exports = async (ctx, next) => {
   ctx.set('Content-Type', 'application/json;charset=utf-8;')
   // 设置客户端访问权限（解决浏览器跨域问题）
   ctx.set('Access-Control-Allow-Origin', '*')
+  ctx.set("Access-Control-Allow-Headers", "X-Token, Content-Type");
   ctx.set('Access-Control-Allow-Methods', 'OPTIONS, HEAD, GET, POST, PUT, DELETE')
 
   await next()
