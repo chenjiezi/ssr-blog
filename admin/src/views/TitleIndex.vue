99f004412abcb09de7bd06fd64aff99b57e487f3
chenjiezi <892774940@qq.com>
2021-07-31 10:23:01 +0800

    add: 文章索引列表配置页面

--- admin/src/views/TitleIndex.vue
+++ admin/src/views/TitleIndex.vue
@@ -1,7 +1,13 @@
-<template>
-  <div id="TitleIndex">
+
+  <template>
+  <div class="container">
+    <el-button
+      size="small"
+      type="primary"
+      class="add-btn"
+      @click="handleCreateOne">新增</el-button>
     <el-table
-      :data="tableData"
+      :data="menuList"
       style="width: 100%;margin-bottom: 20px;"
       row-key="id"
       border
@@ -9,67 +15,83 @@
       default-expand-all
       :tree-props="{children: 'children', hasChildren: 'hasChildren'}">
       <el-table-column
-        prop="date"
-        label="日期"
-        sortable
+        prop="id"
+        label="ID"
         width="180">
       </el-table-column>
       <el-table-column
-        prop="name"
-        label="姓名"
-        sortable
-        width="180">
+        prop="title"
+        label="标题"
+         width="180">
       </el-table-column>
       <el-table-column
-        prop="address"
-        label="地址">
+        prop="articleTitle"
+        label="文章">
+      </el-table-column>
+      <el-table-column
+        prop="hasContent"
+        label="是否展示">
+      </el-table-column>
+      <el-table-column
+        prop="remark"
+        label="备注">
+      </el-table-column>
+      <el-table-column label="操作" width="220">
+        <template slot-scope="scope">
+          <el-button
+            size="mini"
+            type="primary"
+            @click="handleCreate(scope.$index, scope.row)">新增</el-button>
+          <el-button
+            size="mini"
+            @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+          <el-button
+            size="mini"
+            type="danger"
+            @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+        </template>
       </el-table-column>
     </el-table>
   </div>
 </template>
 
 <script>
+  import { fecthMenuList } from '@/api/menu'
+  
   export default {
     name: 'TitleIndex',
     data () {
       return {
-        tableData: [{
-          id: 1,
-          date: '2016-05-02',
-          name: '王小虎2',
-          address: '上海市普陀区金沙江路 1518 弄'
-        }, {
-          id: 2,
-          date: '2016-05-04',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1517 弄'
-        }, {
-          id: 3,
-          date: '2016-05-01',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1519 弄',
-          children: [{
-              id: 31,
-              date: '2016-05-01',
-              name: '王小虎',
-              address: '上海市普陀区金沙江路 1519 弄'
-            }, {
-              id: 32,
-              date: '2016-05-01',
-              name: '王小虎',
-              address: '上海市普陀区金沙江路 1519 弄'
-          }]
-        }, {
-          id: 4,
-          date: '2016-05-03',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1516 弄'
-        }]
+        menuList: []
+      }
+    },
+    mounted () {
+      fecthMenuList().then(res => {
+        this.menuList = res.data.menuList
+      })
+    },
+    methods: {
+      handleCreateOne () {
+        console.log('新增一级索引')
+      },
+      handleCreate (index, row) {
+        console.log('新增 - index, row:', index, row)
+      },
+      handleEdit (index, row) {
+        console.log('编辑 - index, row:', index, row)
+      },
+      handleDelete (index, row) {
+        console.log('删除 - index, row:', index, row)
       }
     }
   }
 </script>
 
 <style lang="scss" scoped>
-  
+  .container {
+    margin: 20px;
+    .add-btn {
+      margin-bottom: 12px;
+    }
+  }
 </style>
