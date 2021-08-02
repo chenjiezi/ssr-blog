7c54b9c27e0160b631996c1672affb8d5cc880e4
chenjiezi <892774940@qq.com>
2021-08-02 21:40:51 +0800

    add: 开发menu接口

--- admin/src/api/menu.js
+++ admin/src/api/menu.js
@@ -1,8 +1,37 @@
 import request from '@/utils/request'
 
-export function fecthMenuList () {
+export function fecthMenuList (params) {
   return request({
     url: '/api/menu/list',
-    method: 'get'
+    method: 'get',
+    params
+  })
+}
+export function fetchMenu (params) {
+  return request({
+    url: '/api/menu/detail',
+    method: 'get',
+    params
+  })
+}
+export function createMenu (data) {
+  return request({
+    url: '/api/menu/create',
+    method: 'post',
+    data
+  })
+}
+export function editMenu (data) {
+  return request({
+    url: '/api/menu/edit',
+    method: 'put',
+    data
+  })
+}
+export function deleteMenu (params) {
+  return request({
+    url: '/api/menu/delete',
+    method: 'delete',
+    params
   })
 }
