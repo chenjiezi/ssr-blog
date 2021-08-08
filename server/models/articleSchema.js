6232f9d4fced9d88eff24259529338e6e73bbcf4
chenjiezi <892774940@qq.com>
2021-08-08 23:44:38 +0800

    feat: article

--- server/models/articleSchema.js
+++ server/models/articleSchema.js
@@ -1,8 +1,16 @@
 const mongoose = require('mongoose')
 
 const articleSchema = mongoose.Schema({
-  'userId': String,
-  'username': String
+    'id': String,
+    'title': String,
+    'content': String,
+    'content_short': String,
+    'path': String,
+    'anchor': String,
+    'status': String,
+    'image_uri': String,
+    'createTime': Date,
+    'remark': String
 })
 
 module.exports = mongoose.model('article', articleSchema, 'article')
