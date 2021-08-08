e00d6e2c976e9dd583fd6884b6e3cb52acd5cce4
chenjiezi <892774940@qq.com>
2021-08-08 23:44:18 +0800

    menu接口对接数据库

--- server/models/menuSchema.js
+++ server/models/menuSchema.js
@@ -1,7 +1,18 @@
 const mongoose = require('mongoose')
 
 const menuSchema = mongoose.Schema({
-  
-})
+  'id': String,
+  'pId': String,
+  'articleId': String,
+  'title': String,
+  'hasContent': Boolean,
+  'articleTitle': String,
+  'createTime': {
+    type: Date,
+    default: new Date()
+  },
+  'updateTime': Date,
+  'remark': String
+}, { versionKey: false })
 
 module.exports = mongoose.model('menu', menuSchema, 'menu')
--- server/router/menu.js
+++ server/router/menu.js
@@ -1,21 +1,14 @@
 const utils = require('../utils/utils')
-
-function fetchData () {
-  return utils.operationFile('../mock/menu.json')
-}
-
-function saveData (fn) {
-  utils.operationFile('../mock/menu.json', fn)
-}
+const Menu = require('../models/menuSchema')
+const Counter = require('../models/counterSchema')
 
 // 文章索引目录
 const menuList = {
   method: 'get',
   path: '/menu/list',
-  fn: (ctx) => {
-    let data = fetchData()
-    data = utils.treeData(data) // 将数据转成树结构
-
+  fn: async (ctx) => {
+    const res = await Menu.find({})
+    const data = utils.treeData(res) // 转成树结构
     ctx.response.body = utils.resBody({
       data: { 
         menuList: data
@@ -27,14 +20,13 @@ const menuList = {
 const menuDetail = {
   method: 'get',
   path: '/menu/detail',
-  fn: (ctx) => {
-    const { id } = utils.getUrlParams(ctx.request.url)
-    const data = fetchData()
+  fn: async (ctx) => {
+    const { id } = ctx.request.query
 
-    const item = data.find(item => item.id === id) || null
-
+    const res = await Menu.findOne({ id })
+    
     ctx.response.body = utils.resBody({
-      data: item
+      data: res
     })
   }
 }
@@ -42,59 +34,76 @@ const menuDetail = {
 const menuEdit = {
   method: 'put',
   path: '/menu/edit',
-  fn: (ctx) => {
+  fn: async (ctx) => {
     const menuData = ctx.request.body
 
-    saveData((data) => {
-      data.forEach((item, index, arr) => {
-        if (item.id === menuData.id) {
-          // 只编辑修改的变量
-          arr[index] = Object.assign(arr[index], menuData)
-        }
+    try {
+      Menu.updateOne({ id: menuData.id }, { $set: menuData })
+      ctx.response.body = utils.resBody({message: '编辑成功!' })
+    } catch (error) {
+      ctx.response.body = utils.resBody({
+        code: '50000',
+        message: error.message
       })
-      return data
-    })
-
-    ctx.response.body = utils.resBody({
-      message: '编辑成功!'
-    })
+    }
   }
 }
 
 const menuCreate = {
   method: 'post',
   path: '/menu/create',
-  fn: (ctx) => {
+  fn: async (ctx) => {
     const menuData = ctx.request.body
 
-    saveData((data) => {
-      // TODO: 自增id
-      const id = data.length + 1
-      menuData.id = id + ''
-      data.push(menuData)
-      return data
-    })
+    try {
+      const res = await Menu.findOne({ title: menuData.title }, 'id title')
+    
+      if (res) {
+        // 1.是否 title 已存在
+        ctx.response.body = utils.resBody({
+          code: '20001',
+          message: `系统检测到有重复的标题索引，信息如下： ${res.id} - ${res.title}`
+        })
+      } else {
+        // 2.生成id
+        const doc = await Counter.findOneAndUpdate(
+          { _id: 'menuId' },
+          { $inc: { sequence_value: 1 } },
+          { new: true }
+        )
+        // 3.插入数据库
+        const menu = new Menu({
+          id: doc.sequence_value,
+          ...menuData
+        })
+        menu.save()
+        ctx.response.body = utils.resBody({ message: '新增成功!' })
+      }
+    } catch (error) {
+      ctx.response.body = utils.resBody({
+        code: '50000',
+        message: error.message
+      })
+    }
 
-    ctx.response.body = utils.resBody({
-      message: '新增成功!'
-    })
   }
 }
 
 const menuDelete = {
   method: 'delete',
   path: '/menu/delete',
-  fn: (ctx) => {
-    const { id } = utils.getUrlParams(ctx.request.url)
+  fn: async (ctx) => {
+    const { id } = ctx.request.query
 
-    saveData((data) => {
-      return data.filter(item => item.id !== id)
-    })
-
-    ctx.response.body = utils.resBody({
-      data: id,
-      message: '删除成功!'
-    })
+    try {
+      const res = await Menu.deleteOne({ id })
+      ctx.response.body = utils.resBody({ message: '删除成功!' })
+    } catch (error) {
+      ctx.response.body = utils.resBody({
+        code: '50000',
+        message: error.message
+      })
+    }
   }
 }
 
