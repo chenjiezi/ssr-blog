bd79fe33b55759856e9a5665f69c1cf9383885d0
chenjiezi <892774940@qq.com>
2021-08-11 22:46:25 +0800

    chore: 代码优化

--- server/router/menu.js
+++ server/router/menu.js
@@ -4,7 +4,8 @@ const req = require('./handler-request')
 const { query, create, detail, edit, del } = req
 
 const menuList = { method: 'get', path: '/menu/list', fn: query(Menu, (res, utils) => {
-  return utils.treeData(JSON.parse(JSON.stringify(res))) // 转成树结构
+  const cloneData = JSON.parse(JSON.stringify(res))
+  return utils.treeData(cloneData) // 转成树结构
 })}
 const menuDetail = { method: 'get', path: '/menu/detail', fn: detail(Menu) }
 const menuEdit = { method: 'put', path: '/menu/edit', fn: edit(Menu) }
